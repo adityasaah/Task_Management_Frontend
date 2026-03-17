5a52e1bebfff77238e94b989578059b59891190b
MyFirstRepo <adityasaah932@gmail.com>
2026-03-17 21:41:46 +0530

    Test : added msw for integration test of createTaskForm

--- src/components/tests/CreateTaskForm.test.tsx
+++ src/components/tests/CreateTaskForm.test.tsx
@@ -1,14 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import CreateTaskForm from '../CreateTaskForm';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
 import { waitFor } from '@testing-library/react';
-import { submitTask } from '../services/tasks.service';
-import type { AxiosResponse } from 'axios';
+import * as tasksService from '../services/tasks.service';
+// import type { AxiosResponse } from 'axios';
+import { server } from '../../mocks/node';
 
 
+interface TaskFormData {
+    title: string;
+    description: string;
+    currentProgress: string;
+    targetProgress: string;
+    metric: string;
+}
 
-const VALID_FORM_DATA = {
+
+const VALID_FORM_DATA : TaskFormData = {
     title: 'Test Task',
     description: 'Test Description',
     currentProgress: '0',
@@ -16,18 +25,31 @@ const VALID_FORM_DATA = {
     metric: 'hours',
 };
 
-vi.mock('../services/tasks.service', () => ({
-    submitTask: vi.fn()
-}));
+const IN_VALID_FORM_DATA = {
+    title: 'Test Task 2',
+    description: 'Test Description',
+    currentProgress: '0',
+    targetProgress: '10',
+    metric: 'hours',
+};
 
-async function fillForm(user: ReturnType<typeof userEvent.setup>) {
-    await user.type(screen.getByLabelText(/title/i), VALID_FORM_DATA.title);
-    await user.type(screen.getByLabelText(/description/i), VALID_FORM_DATA.description);
-    await user.type(screen.getByLabelText(/metric/i), VALID_FORM_DATA.metric);
+const VALID_FORM_DATA_2 = {
+    title: 'Test Task 3',
+    description: 'Test Description',
+    currentProgress: '0',
+    targetProgress: '10',
+    metric: 'hours',
+};
+
+
+async function fillForm(user: ReturnType<typeof userEvent.setup>, formData: TaskFormData) {
+    await user.type(screen.getByLabelText(/title/i), formData.title);
+    await user.type(screen.getByLabelText(/description/i), formData.description);
+    await user.type(screen.getByLabelText(/metric/i), formData.metric);
 
     const targetInput = screen.getByLabelText(/target progress/i);
     await user.clear(targetInput);
-    await user.type(targetInput, VALID_FORM_DATA.targetProgress);
+    await user.type(targetInput, formData.targetProgress.toString());
 }
 
 async function submitForm(user: ReturnType<typeof userEvent.setup>) {
@@ -43,6 +65,10 @@ describe('CreateTaskForm', () => {
         vi.clearAllMocks();
     });
 
+    beforeAll(() => server.listen())
+    afterEach(() => server.resetHandlers())
+    afterAll(() => server.close())
+
     it('renders the form with all required fields', () => {
         const onClose = vi.fn();
         const refetch = vi.fn();
@@ -57,18 +83,19 @@ describe('CreateTaskForm', () => {
 
 
     it('should submits the correct payload', async () => {
-        vi.mocked(submitTask).mockResolvedValueOnce({ data: {} } as AxiosResponse);
-        const user = userEvent.setup();
+         const submitTaskSpy = vi.spyOn(tasksService, 'submitTask');
 
+        const user = userEvent.setup();
         const onClose = vi.fn();
         const refetch = vi.fn();
 
         render(<CreateTaskForm onClose={onClose} refetch={refetch} />);
-        await fillForm(user);
+
+        await fillForm(user, VALID_FORM_DATA);
         await submitForm(user);
 
         await waitFor(() => {
-            expect(submitTask).toHaveBeenCalledWith({
+            expect(submitTaskSpy).toHaveBeenCalledWith({
                 title: VALID_FORM_DATA.title,
                 description: VALID_FORM_DATA.description,
                 currentProgress: Number(VALID_FORM_DATA.currentProgress),
@@ -76,18 +103,20 @@ describe('CreateTaskForm', () => {
                 metric: VALID_FORM_DATA.metric,
             });
         });
+
+        submitTaskSpy.mockRestore();
     });
 
 
     it('calls refetch and onClose after successful form submission', async () => {
-        vi.mocked(submitTask).mockResolvedValueOnce({ data: {} } as AxiosResponse);
+        // vi.mocked(submitTask).mockResolvedValueOnce({ data: {} } as AxiosResponse);
         const user = userEvent.setup();
 
         const onClose = vi.fn();
         const refetch = vi.fn();
 
         render(<CreateTaskForm onClose={onClose} refetch={refetch} />);
-        await fillForm(user);
+        await fillForm(user, VALID_FORM_DATA);
         await submitForm(user);
 
         await waitFor(() => {
@@ -97,14 +126,14 @@ describe('CreateTaskForm', () => {
     });
 
     it('does not call onClose or refetch when submission fails', async () => {
-        vi.mocked(submitTask).mockRejectedValueOnce(new Error('Network error'));
+        // vi.mocked(submitTask).mockRejectedValueOnce(new Error('Network error'));
         const user = userEvent.setup();
 
         const onClose = vi.fn();
         const refetch = vi.fn();
 
         render(<CreateTaskForm onClose={onClose} refetch={refetch} />);
-        await fillForm(user);
+        await fillForm(user,IN_VALID_FORM_DATA);
         await submitForm(user);
 
         await waitFor(() => {
@@ -115,7 +144,7 @@ describe('CreateTaskForm', () => {
 
 
     it('disables the submit button while submitting', async () => {
-        vi.mocked(submitTask).mockReturnValue(new Promise(() => { })); // never resolves
+        // vi.mocked(submitTask).mockReturnValue(new Promise(() => { })); // never resolves
         const user = userEvent.setup();
 
         const onClose = vi.fn();
@@ -123,14 +152,14 @@ describe('CreateTaskForm', () => {
 
 
         render(<CreateTaskForm onClose={onClose} refetch={refetch} />);
-        await fillForm(user);
+        await fillForm(user, VALID_FORM_DATA_2);
         await submitForm(user);
 
         expect(screen.getByRole('button', { name: /creating/i })).toBeDisabled();
     });
 
     it('calls the onClose function on clicking cancel button', async () => {
-        vi.mocked(submitTask).mockResolvedValueOnce({ data: {} } as AxiosResponse); // never resolves
+        // vi.mocked(submitTask).mockResolvedValueOnce({ data: {} } as AxiosResponse); // never resolves
         const user = userEvent.setup();
 
         const onClose = vi.fn();
@@ -138,7 +167,7 @@ describe('CreateTaskForm', () => {
 
 
         render(<CreateTaskForm onClose={onClose} refetch={refetch} />);
-        await fillForm(user);
+        await fillForm(user, VALID_FORM_DATA);
         await clickCancel(user);
 
         await waitFor(() => {
